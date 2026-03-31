771b29cf8416697ed9d974352b35f6b94231d661
Michał Startek <michal.startek@mimuw.edu.pl>
2026-03-31 09:20:07 +0200

    Initial module skeleton

---
+++ reinstall.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+pip uninstall -y wnetdeconv
+VERBOSE=1 pip install -v -e .
---
+++ src/wnetdeconv/__init__.py
@@ -0,0 +1,9 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+from . import wnetdeconv_cpp
+
+
+def hello():
+    print("Hello, world from wnetdeconv!")
